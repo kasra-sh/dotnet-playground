ac66ee7ca270ca1664c1863b7bdeea93c50dbd69
Kasra Shamsaei <kasra_sh94@protonmail.com>
2023-09-10 03:02:26 +0330

    clean up & add application lifetime

--- Framework/Infant.Host/WebApplicationExtensions.cs
+++ Framework/Infant.Host/WebApplicationExtensions.cs
@@ -7,28 +7,64 @@ using AutofacServiceProviderFactory = Infant.Core.Modularity.AutofacServiceProvi
 
 namespace Infant.Host;
 
+public class WebApplicationSettings
+{
+    public bool AddSwaggerUiForDevelopment { get; set; } = false;
+    public bool AddSwaggerUiForProduction { get; set; } = false;
+    public bool AddControllers { get; set; } = false;
+}
+
 public static class WebApplicationExtensions
 {
-    public static Task<WebApplication> InitializeApplicationAsync(this WebApplication webApplication)
-    {
-        // call lifecycle methods
-        return Task.FromResult(webApplication);
-    }
-
     public static async Task AddApplicationAsync<TModule>(
         this WebApplicationBuilder webAppBuilder)
     {
         var containerBuilder = new ContainerBuilder();
         webAppBuilder.Services.AddSingleton(containerBuilder);
         webAppBuilder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(containerBuilder));
-            // .ConfigureServices((context, services) =>
-            // {
-            // });
-        webAppBuilder.Services.AddApplicationModule<TModule>();
-        
-        webAppBuilder.Services.AddControllers();
-        
-        webAppBuilder.Services.AddEndpointsApiExplorer();
-        webAppBuilder.Services.AddSwaggerGen();
+
+        webAppBuilder.Services.AddApplicationModule<TModule>(webAppBuilder);
     }
+    
+    public static async Task InitializeApplicationAsync(this WebApplication app)
+    {
+        var appModules = app.Services.GetServices<AppModule>();
+
+        foreach (var appModule in appModules)
+        {
+            if (CallAndCheckIsNotImplemented(() => appModule.OnApplicationInitialization(app.Services)))
+            {
+                await CallAndCheckIsNotImplementedAsync(async () => await appModule.OnApplicationInitializationAsync(app.Services));
+            }
+
+            app.Lifetime.ApplicationStopping.Register(() => { CallAndCheckIsNotImplemented(() => appModule.OnApplicationShutdown(app.Services)); });
+        }
+    }
+    
+    private static bool CallAndCheckIsNotImplemented(Action action)
+    {
+        try
+        {
+            action.Invoke();
+            return false;
+        }
+        catch (NotImplementedException)
+        {
+            return true;
+        }
+    }
+    
+    private static async Task<bool> CallAndCheckIsNotImplementedAsync(Func<Task> action)
+    {
+        try
+        {
+            await action.Invoke();
+            return false;
+        }
+        catch (NotImplementedException)
+        {
+            return true;
+        }
+    }
+
 }
