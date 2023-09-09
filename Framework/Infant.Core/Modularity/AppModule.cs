bd205eb2969fcda22b1c689a110c1a492dc6eaf0
Kasra Shamsaei <kasra_sh94@protonmail.com>
2023-09-10 03:10:33 +0330

    add OnApplicationInitialization, OnApplicationShutdown

--- Framework/Infant.Core/Modularity/AppModule.cs
+++ Framework/Infant.Core/Modularity/AppModule.cs
@@ -4,9 +4,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Infant.Core.Modularity;
 
-public abstract class AppModule : IAppModule
+public abstract class AppModule
 {
-    public virtual bool AutoRegisterDependencies { get; } = true;
+    public virtual bool AutoRegisterDependencies => true;
+
     public virtual void ConfigureServices(IServiceCollection services)
     {
         ConfigureServicesAsync(services).ConfigureAwait(false).GetAwaiter().GetResult();
@@ -15,5 +16,20 @@ public abstract class AppModule : IAppModule
     public virtual Task ConfigureServicesAsync(IServiceCollection services)
     {
         return Task.CompletedTask;
+    }
+    
+    public virtual void OnApplicationInitialization(IServiceProvider services)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual async Task OnApplicationInitializationAsync(IServiceProvider services)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void OnApplicationShutdown(IServiceProvider services)
+    {
+        throw new NotImplementedException();
     }
 }
