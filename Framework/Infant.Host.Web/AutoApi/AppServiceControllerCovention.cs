a27df4e710373f9701c8f56eb0412ab07f78dd41
Kasra Shamsaei <kasra_sh94@protonmail.com>
2025-06-28 23:15:33 +0330

    implement conventional api from AppService

---
+++ Framework/Infant.Host.Web/AutoApi/AppServiceControllerCovention.cs
@@ -0,0 +1,83 @@
+
+
+using Infant.Core.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+public class ActionProps
+{
+    public bool Conventional = true;
+}
+
+public class ApiDescProvider: IApiDescriptionProvider
+{
+    public void OnProvidersExecuting(ApiDescriptionProviderContext context)
+    {
+        // throw new NotImplementedException();
+    }
+
+    public void OnProvidersExecuted(ApiDescriptionProviderContext context)
+    {
+        foreach (var apiDescription in context.Results)
+        {
+            if (apiDescription.HttpMethod == null)
+            {
+                var methods = apiDescription.ActionDescriptor.EndpointMetadata.Where(em => em is HttpMethodMetadata);
+                apiDescription.HttpMethod = (methods.FirstOrDefault() as HttpMethodMetadata)?.HttpMethods?[0];
+            }
+            
+        }
+        
+    }
+
+    public int Order { get; }
+}
+
+public class AppServiceControllerConvention : IControllerModelConvention
+{
+    public void Apply(ControllerModel controller)
+    {
+        if (typeof(IAppService).IsAssignableFrom(controller.ControllerType))
+        {
+            // Apply [ApiController] and default route
+            controller.Filters.Add(new ApiControllerAttribute());
+            controller.Selectors.Add(new SelectorModel
+            {
+                AttributeRouteModel = new AttributeRouteModel(
+                    // new RouteAttribute($"api/{controller.ControllerName.Replace("AppService", "").Replace("Service", "")}/[action]")
+                    new RouteAttribute($"api/{CreateRouteFromName(controller.ControllerName)}")
+                    {
+                        // Name = controller.ControllerName.Replace("AppService", "").Replace("Service", "")
+                    })
+                {
+                    // Name = controller.ControllerName.Replace("AppService", "").Replace("Service", "")
+                }
+            });
+            foreach (var controllerControllerProperty in controller.ControllerProperties)
+            {
+                var propType = controllerControllerProperty.PropertyInfo.PropertyType;
+                if (propType.IsInterface || propType.IsAbstract)
+                {
+                    controllerControllerProperty.BindingInfo = new BindingInfo
+                    {
+                        BindingSource = BindingSource.Services
+                    };
+                }
+            }
+        }
+    }
+
+    private string CreateRouteFromName(string name)
+    {
+        return ToKebabCase(name.Replace("AppService", "").Replace("Service", ""));
+    }
+    
+    private static string ToKebabCase(string input)
+    {
+        if (string.IsNullOrEmpty(input)) return input;
+        return string.Concat(input.Select((c, i) =>
+            i > 0 && char.IsUpper(c) ? "-" + c.ToString() : c.ToString())).ToLower();
+    }
+}
---
+++ Framework/Infant.Host.Web/AutoApi/AppServiceControllerFeatureProvider.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using Infant.Core.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace Infant.Host.AutoApi;
+
+public class AppServiceControllerFeatureProvider : ControllerFeatureProvider
+{
+    protected override bool IsController(TypeInfo typeInfo)
+    {
+        return typeof(IAppService).IsAssignableFrom(typeInfo) &&
+               typeInfo is { IsAbstract: false, IsClass: true, ContainsGenericParameters: false } &&
+               !typeInfo.IsDefined(typeof(NonControllerAttribute), inherit: true);
+    }
+}
